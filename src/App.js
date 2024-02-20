73141a4bc46243033f4c70b4a208cf8f4662e511
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:43:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+/**
+ * This is the main component of the application.
+ * It renders a simple "Hello World" message.
+ */
 function App() {
   return (
-      <h1>Hello World</h1>
+    <h1>Hello World</h1>
   );
 }
 
