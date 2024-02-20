48ce7b7517e21222dbec1d3a28580785063a3771
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 13:44:05 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Create a root element for React rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside React StrictMode for additional checks and warnings in development mode
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-
-
-
